d9552f4d0a482180177df0fd738ebcac629944b2
Maksym Kaspriv <mkaspriv_cpg@medable.com>
2023-08-23 20:07:47 +0200

    feat: finish basic api

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,7 +1,10 @@
-import { getMovies } from "./storage";
+import { getMovies, getMovie, getSuggestions } from "./storage";
 
 export const resolvers = {
   Query: {
-    movies: () => getMovies(),
-  }
-}
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
+  },
+};
+
