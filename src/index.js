32c91c5cdf8071454fc8d4cd171bc0f33458a940
Maksym Kaspriv <mkaspriv_cpg@medable.com>
2023-08-27 13:28:30 +0200

    fix: fix port in env

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const yoga = new createYoga({
 });
 
 const server = createServer(yoga);
-
-server.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.info(`Server is running on port ${port}`)
 })
 
